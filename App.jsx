3c78b18f15e080825c94de82e0abf60f3d70da2e
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-30 19:25:55 -0300

    fix: app configuration

--- App.jsx
+++ App.jsx
@@ -1,11 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Routes/>
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from "@react-navigation/native";
 import TabRoutes from "./tab.routes";
+import { StatusBar } from "expo-status-bar";
 
 export default function Routes() {
     return(
         <NavigationContainer>
             <TabRoutes/>
+            <StatusBar style="auto"/>
         </NavigationContainer>
     );
 }
