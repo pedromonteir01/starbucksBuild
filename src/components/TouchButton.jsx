b12c53384dd0a089e9ce3d92b02b4031c7b3dee5
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-30 19:52:49 -0300

    feat: TouchButton component

---
+++ src/components/TouchButton.jsx
@@ -0,0 +1,20 @@
+import { useNavigation } from "@react-navigation/native";
+import { Text, TouchableOpacity } from "react-native";
+
+
+const TouchButton = ({ title, route, data }) => {
+
+    const navigation = useNavigation();
+
+    return(
+        <>
+            <TouchableOpacity onPress={navigation.navigate(route, {data: data})}> 
+                <Text>
+                    {title}
+                </Text>
+            </TouchableOpacity>
+        </>
+    );
+}
+
+export default TouchButton;
