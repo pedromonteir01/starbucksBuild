7610d3922f6e56a008094b8289768abd2ffdb739
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-30 19:36:18 -0300

    feat: test tailwind and basic style from home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Home() {
     return(
-        <View>
+        <View className='items-center justify-center flex p-10'>
             <Text>Olá Mundo!</Text>
         </View>
     );
