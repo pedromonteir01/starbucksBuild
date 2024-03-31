777a1258801e44d9058dcead55c9559b38574684
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-31 10:10:45 -0300

    fix: babel error (tailwind plataform selector)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 
-const { plataformSelect } = require('nativewind');
-
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        default: '#006241',
-        error: plataformSelect({
+        defaultColor: '#006241',
+        /* error: plataformSelect({
           ios: 'plataformColor(SystemRed)',
           android: 'plataformColor(?android:colorError)',
-          default: 'red'
-        })
+          defaultColor: 'red' 
+        })*/
       }
     },
   },
