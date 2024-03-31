df08fc882edd86a3ed190471edda7028d514991c
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-31 01:48:45 -0300

    feat: menu structure and tab navigator

--- src/routes/tab.routes.jsx
+++ src/routes/tab.routes.jsx
@@ -2,21 +2,30 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 //pages
 import Home from "../screens/Home";
+import Menu from "../screens/Menu";
 
 export default function TabRoutes() {
 
     const Tab = createBottomTabNavigator();
 
-    return(
+    return (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen 
-                name="Home" 
+            <Tab.Screen
+                name="Home"
                 component={Home}
                 options={{
                     tabBarLabel: 'Home',
                     tabBarActiveTintColor: "#131313",
                     tabBarInactiveTintColor: "#D6D6D6",
-                }}/>
+                }} />
+            <Tab.Screen
+                name="Menu"
+                component={Menu}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarActiveTintColor: "#131313",
+                    tabBarInactiveTintColor: "#D6D6D6",
+                }} />
         </Tab.Navigator>
     );
 }
---
+++ src/screens/Menu.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Menu() {
+    return(
+        <View>
+            <Text>Opa</Text>
+        </View>
+    );
+}
