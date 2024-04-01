8188358495defa99f7c7e3ec4dc04f758b27b344
ElPedron <pedrohenriquesilva@aluno.senai.br>
2024-03-31 22:12:37 -0300

    finish day

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,9 @@
-import { Image, View } from "react-native"
+import { Image, View, Text } from "react-native"
 
 const Card = ({ photo, name, price }) => {
     return(
-        <View>
-            <Image source={photo}/>
+        <View className='border-2 border-black'>
+            <Image source={{uri: photo}} className='w-1/2 h-1/2'/>
             <View>
                 <Text>{name}</Text>
                 <Text>{price}</Text>
--- src/screens/Menu.jsx
+++ src/screens/Menu.jsx
@@ -1,13 +1,29 @@
 import { ScrollView, Text, View } from "react-native";
 
+//components
+import Card from "../components/Card";
+
+const test = [
+    {
+        id: 1,
+        image: '../../assets/images/logo.png',
+        name: 'test1',
+        price: '19.99'
+    },
+]
+
 export default function Menu() {
     return(
         <View className='flex w-full h-full p-12'>
             {/* Cada item */}
-            <View>
+            <View className='w-full h-28'>
                 <Text>Bebidas</Text>
-                <ScrollView>
-                    
+                <ScrollView className='h-full'>
+                    {
+                        test.map((food) => (
+                            <Card name={food.name} photo={food.image} price={food.price} key={food.id}/>
+                        ))
+                    }
                 </ScrollView>
             </View>
         </View>
